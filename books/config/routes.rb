5feefbbb219cb023bd9f3acc7fb526e6cae21ae5
ogawayouji <satsumaimo697@gmail.com>
2021-12-05 23:35:19 +0900

    feat: initial commit

---
+++ books/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # get 'home/index'
+  root to: 'home#index'
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+  post '/graphql', to: 'graphql#execute'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ typescript-test/app/javascript/packs/hello_react.tsx
@@ -0,0 +1,34 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import PropTypes from 'prop-types'
+
+interface HelloProps {
+  name: string
+}
+
+// const Hello = props => (
+//   <div>Hello {props.name}!</div>
+// )
+
+const Hello: React.SFC<HelloProps> = props => (
+  <div>Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
